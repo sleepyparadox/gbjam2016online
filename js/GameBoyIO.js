1a5833f9effa0539487bb05e12c0d48956321c12
don <sleepyparadox@gmail.com>
2016-10-10 00:15:19 +1100

    fixed blending in graphics and made index.html the launching page

--- js/GameBoyIO.js
+++ js/GameBoyIO.js
@@ -2,10 +2,10 @@
 var gameboy = null;						//GameBoyCore object.
 var gbRunInterval = null;				//GameBoyCore Timer
 var settings = [						//Some settings.
-	true, 								//Turn on sound.
+	false, 								//Turn on sound.
 	true,								//Boot with boot ROM first?
 	false,								//Give priority to GameBoy mode
-	1,									//Volume level set.
+	0,									//Volume level set.
 	true,								//Colorize GB mode?
 	false,								//Disallow typed arrays?
 	8,									//Interval for the emulator loop.
@@ -15,7 +15,7 @@ var settings = [						//Some settings.
 	false,								//Override MBC RAM disabling and always allow reading and writing to the banks.
 	false,								//Use the GameBoy boot ROM instead of the GameBoy Color boot ROM.
 	false,								//Scale the canvas in JS, or let the browser scale the canvas?
-	true,								//Use image smoothing based scaling?
+	false,								//Use image smoothing based scaling?
     [true, true, true, true]            //User controlled channel enables.
 ];
 function start(canvas, ROM) {
